6ed844dd3bbd1f667733df93855ae8587875ca04
LucasMtss <lucasrmts@gmail.com>
2023-06-15 18:48:34 -0300

    feat: add initial files

---
+++ src/components/Contacts/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { SectionContent, SectionTitle } from '../../pages/Home/style';
+
+interface IEducationProps {
+    data: string[];
+}
+
+function Contacts({data}: IEducationProps) {
+  return (
+        data.map(contact => {
+            return (
+            <>
+                <SectionTitle>{contact}</SectionTitle>
+            </>
+            )
+        }) 
+  )
+}
+
+export default Contacts
